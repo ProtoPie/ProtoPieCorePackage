cacb922ccc19201314a8493c3f2253dc0cbc0fd9
Michael Kim <michael@protopie.io>
2025-07-15 10:28:04 +0900

    fix: change to copilot's recommend

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(
             name: "ProtoPieCoreStatic",
             targets: ["ProtoPieCoreStatic"]
-        )
+        ),
     ],
     targets: [
         .binaryTarget(
