e3ff7483d5b324237d2c0e065b44148928e009cc
Sookie999 <sookie@protopie.io>
2025-08-21 15:24:20 +0900

    test

--- Package.swift
+++ Package.swift
@@ -8,11 +8,19 @@ let package = Package(
     ],
     products: [
         .library(
+            name: "ProtoPieCoreStatic",
+            targets: ["ProtoPieCoreStatic"]
+        ),
+        .library(
             name: "ProtoPieCore",
             targets: ["ProtoPieCore"]
         ),
     ],
     targets: [
+        .binaryTarget(
+            name: "ProtoPieCoreStatic",
+            path: "./ProtoPieCoreStatic.xcframework"
+        ),
         .binaryTarget(
             name: "ProtoPieCore",
             path: "./ProtoPieCore.xcframework"
